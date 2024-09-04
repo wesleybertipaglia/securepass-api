f4e50ba6d95baa8de851d14130ad33c4772218da
wesleybertipaglia <wesleybertipaglia@gmail.com>
2024-09-04 12:01:23 -0300

    feat: add AuthService

---
+++ src/main/java/com/wesleybertipaglia/securepass/services/AuthService.java
@@ -0,0 +1,69 @@
+package com.wesleybertipaglia.securepass.services;
+
+import java.time.Instant;
+
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+
+import com.wesleybertipaglia.securepass.entities.User;
+import com.wesleybertipaglia.securepass.repositories.UserRepository;
+import com.wesleybertipaglia.securepass.records.auth.SignInRequestRecord;
+import com.wesleybertipaglia.securepass.records.auth.SignInResponseRecord;
+import com.wesleybertipaglia.securepass.records.auth.SignUpRequestRecord;
+import com.wesleybertipaglia.securepass.records.auth.SignUpResponseRecord;
+
+@Service
+public class AuthService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private JwtEncoder jwtEncoder;
+
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
+    @Transactional
+    public SignInResponseRecord signIn(SignInRequestRecord signInRequest) {
+        User user = userRepository.findByEmail(signInRequest.email()).orElseThrow(
+                () -> new BadCredentialsException("Invalid e-mail or password."));
+
+        if (!passwordEncoder.matches(signInRequest.password(), user.getPassword())) {
+            throw new BadCredentialsException("Invalid e-mail or password.");
+        }
+
+        Instant now = Instant.now();
+        Long expirationTime = Long.valueOf(24 * 60 * 60 * 1000);
+
+        JwtClaimsSet jwtClaimsSet = JwtClaimsSet.builder()
+                .issuer("securepass")
+                .subject(user.getId().toString())
+                .issuedAt(now)
+                .expiresAt(now.plusSeconds(expirationTime))
+                .claim("role", "USER")
+                .build();
+
+        String token = jwtEncoder.encode(JwtEncoderParameters.from(jwtClaimsSet)).getTokenValue();
+        return new SignInResponseRecord(token, expirationTime);
+    }
+
+    @Transactional
+    public SignUpResponseRecord signUp(SignUpRequestRecord signUpRequest) {
+        if (userRepository.existsByEmail(signUpRequest.email())) {
+            throw new BadCredentialsException("E-mail already in use.");
+        }
+
+        User user = new User(signUpRequest.name(), signUpRequest.email(),
+                passwordEncoder.encode(signUpRequest.password()));
+
+        userRepository.save(user);
+        return new SignUpResponseRecord(user.getName(), user.getEmail());
+    }
+
+}
