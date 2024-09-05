9c664a2bdc9250a466b55cc7e8c2ffc6148b3cb7
wesleybertipaglia <wesleybertipaglia@gmail.com>
2024-09-05 19:35:56 -0300

    feat: update AuthControllerIntegratedTests
    refactor the tests, separe methods by aaa

--- src/test/java/com/wesleybertipaglia/securepass/tests/controllers/AuthControllerIntegratedTests.java
+++ src/test/java/com/wesleybertipaglia/securepass/tests/controllers/AuthControllerIntegratedTests.java
@@ -1,16 +1,16 @@
 package com.wesleybertipaglia.securepass.tests.controllers;
 
-import static org.mockito.Mockito.when;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import org.mockito.MockitoAnnotations;
+import static org.mockito.Mockito.when;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
-
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -26,7 +26,7 @@ import com.wesleybertipaglia.securepass.services.auth.AuthService;
 
 @WebMvcTest(AuthController.class)
 @Import(SecurityConfig.class)
-public class AuthControllerTests {
+public class AuthControllerIntegratedTests {
 
     @Autowired
     private MockMvc mockMvc;
@@ -44,11 +44,13 @@ public class AuthControllerTests {
 
     @Test
     void shouldSignInSuccessfully() throws Exception {
+        // arrange
         SignInRequestRecord signInRequest = new SignInRequestRecord("test@example.com", "password123");
         SignInResponseRecord signInResponse = new SignInResponseRecord("mockedToken", 86400L);
 
         when(authService.signIn(signInRequest)).thenReturn(signInResponse);
 
+        // act & assert
         mockMvc.perform(post("/auth/signin")
                 .contentType("application/json")
                 .content(objectMapper.writeValueAsString(signInRequest)))
@@ -59,10 +61,13 @@ public class AuthControllerTests {
 
     @Test
     void shouldSignUpSuccessfully() throws Exception {
+        // arrange
         SignUpRequestRecord signUpRequest = new SignUpRequestRecord("Test User", "test@example.com", "password123");
         SignUpResponseRecord signUpResponse = new SignUpResponseRecord("Test User", "test@example.com");
+
         when(authService.signUp(signUpRequest)).thenReturn(signUpResponse);
 
+        // act & assert
         mockMvc.perform(post("/auth/signup")
                 .contentType("application/json")
                 .content(objectMapper.writeValueAsString(signUpRequest)))
@@ -70,5 +75,4 @@ public class AuthControllerTests {
                 .andExpect(jsonPath("$.name").value("Test User"))
                 .andExpect(jsonPath("$.email").value("test@example.com"));
     }
-
 }
