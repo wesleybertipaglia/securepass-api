cbfdaabd88016aedec001fa7742a1f5f456e8be7
wesleybertipaglia <wesleybertipaglia@gmail.com>
2024-09-05 19:23:37 -0300

    feat: update AuthServiceUnitTests
    refactor the tests, separe methods by aaa

--- src/test/java/com/wesleybertipaglia/securepass/tests/services/AuthServiceUnitTests.java
+++ src/test/java/com/wesleybertipaglia/securepass/tests/services/AuthServiceUnitTests.java
@@ -1,24 +1,19 @@
 package com.wesleybertipaglia.securepass.tests.services;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+
 import java.util.Optional;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.jwt.Jwt;
@@ -33,7 +28,12 @@ import com.wesleybertipaglia.securepass.repositories.UserRepository;
 import com.wesleybertipaglia.securepass.services.auth.AuthService;
 import com.wesleybertipaglia.securepass.entities.User;
 
-public class AuthServiceTests {
+public class AuthServiceUnitTests {
+
+    private static final String TEST_EMAIL = "test@example.com";
+    private static final String TEST_PASSWORD = "password123";
+    private static final String ENCODED_PASSWORD = "$2a$10$eW5lCfRMl5Hftg6woBQaWe";
+    private static final UUID USER_ID = UUID.randomUUID();
 
     @InjectMocks
     private AuthService authService;
@@ -54,84 +54,99 @@ public class AuthServiceTests {
 
     @Test
     void shouldSignInSuccessfully() {
-        String email = "test@example.com";
-        String rawPassword = "password123";
-        String encodedPassword = "$2a$10$eW5lCfRMl5Hftg6woBQaWe";
-        UUID userId = UUID.randomUUID();
+        // arrange
+        User user = new User("Test User", TEST_EMAIL, ENCODED_PASSWORD);
+        user.setId(USER_ID);
 
-        User user = new User("Test User", email, encodedPassword);
-        user.setId(userId);
-
-        when(userRepository.findByEmail(email)).thenReturn(Optional.of(user));
-        when(passwordEncoder.matches(rawPassword, encodedPassword)).thenReturn(true);
+        when(userRepository.findByEmail(TEST_EMAIL)).thenReturn(Optional.of(user));
+        when(passwordEncoder.matches(TEST_PASSWORD, ENCODED_PASSWORD)).thenReturn(true);
 
         Jwt jwt = mock(Jwt.class);
         when(jwt.getTokenValue()).thenReturn("mockedToken");
         when(jwtEncoder.encode(any(JwtEncoderParameters.class))).thenReturn(jwt);
 
-        SignInRequestRecord signInRequest = new SignInRequestRecord(email, rawPassword);
+        SignInRequestRecord signInRequest = new SignInRequestRecord(TEST_EMAIL, TEST_PASSWORD);
+
+        // act
         SignInResponseRecord response = authService.signIn(signInRequest);
 
+        // assert
         assertEquals("mockedToken", response.accessToken());
         assertEquals(86400000, response.expiresIn());
     }
 
     @Test
     void shouldThrowExceptionWhenEmailNotFound() {
+        // arrange
         when(userRepository.findByEmail(anyString())).thenReturn(Optional.empty());
-        SignInRequestRecord signInRequest = new SignInRequestRecord("test@example.com", "password");
+        SignInRequestRecord signInRequest = new SignInRequestRecord(TEST_EMAIL, "password");
+
+        // act & assert
         assertThrows(BadCredentialsException.class, () -> authService.signIn(signInRequest));
     }
 
     @Test
     void shouldThrowExceptionWhenPasswordDoesNotMatch() {
-        User user = new User("Test User", "test@example.com", "$2a$10$eW5lCfRMl5Hftg6woBQaWe");
-
-        when(userRepository.findByEmail(user.getEmail())).thenReturn(Optional.of(user));
+        // arrange
+        User user = new User("Test User", TEST_EMAIL, ENCODED_PASSWORD);
+        when(userRepository.findByEmail(TEST_EMAIL)).thenReturn(Optional.of(user));
         when(passwordEncoder.matches(anyString(), anyString())).thenReturn(false);
 
-        SignInRequestRecord signInRequest = new SignInRequestRecord("test@example.com", "wrongpassword");
+        SignInRequestRecord signInRequest = new SignInRequestRecord(TEST_EMAIL, "wrongpassword");
+
+        // act & assert
         assertThrows(BadCredentialsException.class, () -> authService.signIn(signInRequest));
     }
 
     @Test
     void shouldSignUpSuccessfully() {
+        // arrange
         when(userRepository.existsByEmail(anyString())).thenReturn(false);
 
-        SignUpRequestRecord signUpRequest = new SignUpRequestRecord("Test User", "test@example.com", "password123");
+        SignUpRequestRecord signUpRequest = new SignUpRequestRecord("Test User", TEST_EMAIL, TEST_PASSWORD);
         User user = new User(signUpRequest.name(), signUpRequest.email(), "encodedPassword");
 
         when(userRepository.save(any(User.class))).thenReturn(user);
         when(passwordEncoder.encode(anyString())).thenReturn("encodedPassword");
 
+        // act
         SignUpResponseRecord response = authService.signUp(signUpRequest);
 
+        // assert
         assertEquals(signUpRequest.name(), response.name());
         assertEquals(signUpRequest.email(), response.email());
     }
 
     @Test
     void shouldThrowExceptionWhenEmailAlreadyExists() {
+        // arrange
         when(userRepository.existsByEmail(anyString())).thenReturn(true);
-        SignUpRequestRecord signUpRequest = new SignUpRequestRecord("Test User", "test@example.com", "password123");
+        SignUpRequestRecord signUpRequest = new SignUpRequestRecord("Test User", TEST_EMAIL, TEST_PASSWORD);
 
+        // act & assert
         assertThrows(BadCredentialsException.class, () -> authService.signUp(signUpRequest));
     }
 
     @Test
     void shouldDeleteAccountSuccessfully() {
-        UUID userId = UUID.randomUUID();
-        User user = new User("Test User", "test@example.com", "encodedPassword");
-
-        when(userRepository.findById(userId)).thenReturn(Optional.of(user));
+        // arrange
+        User user = new User("Test User", TEST_EMAIL, "encodedPassword");
+        when(userRepository.findById(USER_ID)).thenReturn(Optional.of(user));
         doNothing().when(userRepository).delete(user);
-        authService.deleteAccount(userId.toString());
+
+        // act
+        authService.deleteAccount(USER_ID.toString());
+
+        // assert
         verify(userRepository, times(1)).delete(user);
     }
 
     @Test
     void shouldThrowExceptionWhenAccountNotFound() {
+        // arrange
         when(userRepository.findById(any(UUID.class))).thenReturn(Optional.empty());
+
+        // act & assert
         assertThrows(BadCredentialsException.class, () -> authService.deleteAccount(UUID.randomUUID().toString()));
     }
 
